90caec489acb498fc9cfafd7ead368579e42c686
Kapil Nila <nilakapil1@gmail.com>
2025-10-24 18:07:56 +0530

    WSGI and render.yaml

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,2 @@
 from backend.wsgi import application
 
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
-
-application = get_wsgi_application()
